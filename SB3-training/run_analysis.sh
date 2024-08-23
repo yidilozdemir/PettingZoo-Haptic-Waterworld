f0d4627fd5dea6d6742e7bf0fca732f3e581bb68
yidilozdemir <yagmuriozdemir96@gmail.com>
2024-08-23 18:18:18 +0100

    run the correct fnx

--- SB3-training/run_analysis.sh
+++ SB3-training/run_analysis.sh
@@ -35,7 +35,7 @@ source $PROJECT_DIR/pettingzoo-sb3/bin/activate
 # Run analysis script using Python in activated virtual environment passing in path to
 # directory containing input data and path to directory to write outputs to
 echo "Running analysis script..."
-python $PROJECT_DIR/sb3_waterworld_myriad.py --n_pursuers 2 --haptic_modulation_type average --haptic_weight 0.5 --policy_name MlpLstmPolicy 
+python $PROJECT_DIR/sb3_waterworld_vector.py --n_pursuers 2 --haptic_modulation_type average --haptic_weight 0.5 --policy_name MlpLstmPolicy 
 echo "...done."
 
 # Copy script outputs back to scratch space under a job ID specific subdirectory
