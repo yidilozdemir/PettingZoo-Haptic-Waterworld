b580d732a27ef2cf88c9017beb540b3409f5b558
yidilozdemir <yagmuriozdemir96@gmail.com>
2024-08-24 19:14:54 +0100

    try with changing few arguments

--- SB3-training/run_analysis_no_effect.sh
+++ SB3-training/run_analysis_no_effect.sh
@@ -2,21 +2,11 @@
 
 # Example batch script to run a Python script in a virtual environment.
 
-# Request 1 minutes of wallclock time (format hours:minutes:seconds).
-#$ -l h_rt=0:1:0
-
-# Request 1 gigabyte of RAM for each core/thread 
-# (must be an integer followed by M, G, or T)
-#$ -l mem=1G
-
-# Request 1 gigabyte of TMPDIR space (default is 10 GB)
-#$ -l tmpfs=1G
+# Request 20 gigabyte of TMPDIR space (default is 10 GB)
+#$ -l tmpfs=20G
 
 # Set the name of the job.
-#$ -N python-analysis-example
-
-# Request 1 cores.
-#$ -pe smp 1
+#$ -N pettingzoo
 
 # Set the working directory to project directory in your scratch space.
 # Replace "<your_UCL_id>" with your UCL user ID
@@ -35,7 +25,7 @@ source $PROJECT_DIR/pettingzoo-sb3/bin/activate
 # Run analysis script using Python in activated virtual environment passing in path to
 # directory containing input data and path to directory to write outputs to
 echo "Running analysis script..."
-python $PROJECT_DIR/sb3_waterworld_vector.py --n_pursuers 2 --haptic_modulation_type no_effect --haptic_weight 0.5 --policy_name MlpLstmPolicy >> output_no_effect.txt
+python $PROJECT_DIR/sb3_waterworld_vector.py --n_pursuers 2 --haptic_modulation_type no_effect --haptic_weight 0.5 --policy_name MlpLstmPolicy >> $PROJECT_DIR/output_no_effect.txt
 echo "...done."
 
 # Copy script outputs back to scratch space under a job ID specific subdirectory
