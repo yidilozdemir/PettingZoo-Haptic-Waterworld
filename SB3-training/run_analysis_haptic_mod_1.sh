35a74eb71c56947411fa11828187af441852615a
yidilozdemir <yagmuriozdemir96@gmail.com>
2024-08-28 12:24:06 +0100

    submit two more jobs

--- SB3-training/run_analysis_haptic_mod_1.sh
+++ SB3-training/run_analysis_haptic_mod_1.sh
@@ -4,15 +4,15 @@
 
 #$ -l tmpfs=10G
 
-# Example batch script to run a Python script in a virtual environment.
-#request 24 hours 
-#$ -l h_rt=18:00:00
 
-# For 2 GPUs
-#$ -l gpu=2
+# Example batch script to run a Python script in a virtual environment.
+#request 15 hours 
+#$ -l h_rt=15:00:00
+
+#$ -pe smp 6
 
 # Set the name of the job.
-#$ -N pettingzoo
+#$ -N pettingzoo0.8
 
 # Set the working directory to project directory in your scratch space.
 # Replace "<your_UCL_id>" with your UCL user ID
@@ -31,7 +31,7 @@ source $PROJECT_DIR/pettingzoo-sb3/bin/activate
 # Run analysis script using Python in activated virtual environment passing in path to
 # directory containing input data and path to directory to write outputs to
 echo "Running analysis script..."
-python $PROJECT_DIR/sb3_waterworld_vector.py --n_pursuers 2 --haptic_modulation_type average --haptic_weight 0.8 --policy_name MlpLstmPolicy >> output_average_haptic_weight0.8.txt
+python $PROJECT_DIR/sb3_waterworld_vector.py --n_pursuers 2 --haptic_modulation_type average --haptic_weight 0.8 --policy_name MlpLstmPolicy 
 echo "...done."
 
 # Copy script outputs back to scratch space under a job ID specific subdirectory
