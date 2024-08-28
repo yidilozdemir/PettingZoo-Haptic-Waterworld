16874bed4699454134afcfe3e10f9768539dd44c
yidilozdemir <yagmuriozdemir96@gmail.com>
2024-08-28 13:53:12 +0100

    dont write output txt file takes time

--- SB3-training/run_analysis_no_effect.sh
+++ SB3-training/run_analysis_no_effect.sh
@@ -2,17 +2,17 @@
 
 # Example batch script to run a Python script in a virtual environment.
 
+
 #$ -l tmpfs=10G
 
 # Example batch script to run a Python script in a virtual environment.
 #request 24 hours 
-#$ -l h_rt=18:00:00
+#$ -l h_rt=20:00:00
 
-# For 2 GPUs
-#$ -l gpu=2
+#$ -pe smp 6
 
 # Set the name of the job.
-#$ -N pettingzoo
+#$ -N pettingzoo-noeffect
 
 # Set the working directory to project directory in your scratch space.
 # Replace "<your_UCL_id>" with your UCL user ID
@@ -31,7 +31,7 @@ source $PROJECT_DIR/pettingzoo-sb3/bin/activate
 # Run analysis script using Python in activated virtual environment passing in path to
 # directory containing input data and path to directory to write outputs to
 echo "Running analysis script..."
-python $PROJECT_DIR/sb3_waterworld_vector.py --n_pursuers 2 --haptic_modulation_type no_effect --haptic_weight 0.5 --policy_name MlpLstmPolicy >> $PROJECT_DIR/output_no_effect.txt
+python $PROJECT_DIR/sb3_waterworld_vector.py --n_pursuers 2 --haptic_modulation_type no_effect --haptic_weight 0.5 --policy_name MlpLstmPolicy 
 echo "...done."
 
 # Copy script outputs back to scratch space under a job ID specific subdirectory
