6d2712fcf391fdf51063f4e59cac83ba89dab336
yidilozdemir <yagmuriozdemir96@gmail.com>
2024-09-02 14:19:45 +0100

    run just waterworld_v4 with recurrentPPO

---
+++ SB3-training/run_analysis_no_haptic.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -l
+
+# Example batch script to run a Python script in a virtual environment.
+
+#$ -l tmpfs=10G
+
+# Example batch script to run a Python script in a virtual environment.
+#request 22 hours 
+#$ -l h_rt=32:00:00
+
+#$ -pe smp 8
+
+# Set the name of the job.
+#$ -N pettingzoo
+
+# Set the working directory to project directory in your scratch space.
+# Replace "<your_UCL_id>" with your UCL user ID
+#$ -wd /home/zcbtyio/Scratch/PettingZoo-Haptic-Waterworld/SB3-training
+
+# Load python3 module - this must be the same version as loaded when creating and
+# installing dependencies in the virtual environment
+module load python3/3.9
+
+# Define a local variable pointing to the project directory in your scratch space
+PROJECT_DIR=/home/zcbtyio/Scratch/PettingZoo-Haptic-Waterworld/SB3-training
+
+# Activate the virtual environment in which you installed the project dependencies
+source $PROJECT_DIR/pettingzoo-sb3/bin/activate
+
+# Run analysis script using Python in activated virtual environment passing in path to
+# directory containing input data and path to directory to write outputs to
+echo "Running analysis script..."
+python $PROJECT_DIR/sb3_waterworld_nohaptic_vector.py --policy_name MlpLstmPolicy 
+echo "...done."
+
+# Copy script outputs back to scratch space under a job ID specific subdirectory
+echo "Copying analysis outputs to scratch space..."
+rsync -a logs/ $PROJECT_DIR/outputs_$JOB_ID/
+cp output_average_haptic_weight0.5.txt $PROJECT_DIR/outputs_$JOB_ID/
+echo "...done"
